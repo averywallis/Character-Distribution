3da593e3633c1e4f37d838eadde113c434f44cd7
averywallis <averybwallis@gmail.com>
2016-02-22 14:38:58 -0500

    Updated from Brython Server: 2/22/2016 2:38:58 PM

--- distribution.py
+++ distribution.py
@@ -67,6 +67,6 @@ x=string1.count('x')
 y=string1.count('y')
 z=string1.count('z')
 list1=list([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z])
-list1=list1.sort()
+list1=list1.sort(reverse=True)
 print(list1)
 
