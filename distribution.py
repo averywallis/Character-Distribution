26d5b65f2b15da5d95cc2f93b14100f8c8850983
averywallis <averybwallis@gmail.com>
2016-02-22 14:16:27 -0500

    Updated from Brython Server: 2/22/2016 2:16:27 PM

--- distribution.py
+++ distribution.py
@@ -37,6 +37,6 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 import string
-string= str.ascii_lowercase(input("Please enter a string of text (the bigger the better): "))
+string= str(input("Please enter a string of text (the bigger the better): "))
 print('The distribution of characters in "The rain in Spain stays mainly in the plain." is:')
 
