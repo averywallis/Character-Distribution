9b4a616fb7c017ae53a801b92914b4496dfe58ca
averywallis <averybwallis@gmail.com>
2016-02-23 14:49:07 -0500

    Updated from Brython Server: 2/23/2016 2:49:08 PM

--- distribution.py
+++ distribution.py
@@ -84,7 +84,10 @@ for c in alph:
         result.append(t)
         listnum.append(r)
         
-
+thing=list(result)
+thing1=thing.split(' ')
+m=result.split(' ')
+print()
 
 listnum.sort(reverse=True)
 l=len(result)
