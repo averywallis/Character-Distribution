a1600e7bf4f6246dfb10153c059f276a58f7d1c8
averywallis <averybwallis@gmail.com>
2016-02-25 16:00:51 -0500

    Updated from Brython Server: 2/25/2016 4:00:54 PM

--- distribution.py
+++ distribution.py
@@ -58,7 +58,7 @@ for c in alph:
         listnum.append(r)
 l=len(result)
 
-lists=zip(listnum,result)
+lists=zip(listnum, result)
 result=sorted(lists, key=lambda listnum: listnum[0])   # sort by number
 result.sort(reverse=True)
 
