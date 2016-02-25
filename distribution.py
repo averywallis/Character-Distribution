1b11d5cad789c683d4ff3cd3e0d17f721ca8cc9c
averywallis <averybwallis@gmail.com>
2016-02-25 15:48:43 -0500

    Updated from Brython Server: 2/25/2016 3:48:46 PM

--- distribution.py
+++ distribution.py
@@ -62,7 +62,7 @@ lists=zip(listnum,result)
 result=sorted(lists, key=lambda listnum: listnum[0])   # sort by number
 result.sort(reverse=True)
 listnum2, result2=*zip(lists)
-print(result)
+print(result2)
 
 '''
 https://docs.python.org/3/howto/sorting.html#sortinghowto
