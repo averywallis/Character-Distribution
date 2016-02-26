e614a345172d76e63b89b60fc46b613abc23f831
averywallis <averybwallis@gmail.com>
2016-02-26 14:44:50 -0500

    Updated from Brython Server: 2/26/2016 2:44:49 PM

--- distribution.py
+++ distribution.py
@@ -62,7 +62,7 @@ for y in range(0,l-1):
     if len(a[y])==len(a[y+1]) or len(a[y])==len(a[y-1]):
         b==thing.append(a[y])
         thing=thing.sort()
-        print(thing)
+        print(thing[y])
     elif not len(a[y])==len(a[y+1]):
         print(a[y])
           
