3bae03fc07ffce525ab5b3fcaaf295fc66a54eb2
averywallis <averybwallis@gmail.com>
2016-02-26 14:27:12 -0500

    Updated from Brython Server: 2/26/2016 2:27:12 PM

--- distribution.py
+++ distribution.py
@@ -58,6 +58,7 @@ lists=sorted(lists, key=lambda listnum: listnum[0])   # sort by number
 lists.sort(reverse=True)
 print(list(lists))
 l=len([x[1] for x in lists])
+
 for y in range(0,l-1):
     if not y==y+1:
         print([r[1] for r in lists])
