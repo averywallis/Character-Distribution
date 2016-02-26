93bcc835f9295ae51fd9a5ab2071381da4e17511
averywallis <averybwallis@gmail.com>
2016-02-26 14:12:46 -0500

    Updated from Brython Server: 2/26/2016 2:12:46 PM

--- distribution.py
+++ distribution.py
@@ -57,7 +57,7 @@ print(list(lists))
 lists=sorted(lists, key=lambda listnum: listnum[0])   # sort by number
 lists.sort(reverse=True)
 print(list(lists))
-print(list(lists(result[-1])))
+print(list(lists[-1]))
 
 
 '''
